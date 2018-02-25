e6fe26339e791568be376f8475d96290f1d369e0
radisadev <radisadev@gmail.com>
2018-02-25 12:59:46 +0100

    Add utxo.js

--- utxo.js
+++ utxo.js
@@ -1,5 +1,3 @@
-
-
 function UTXO(txHash, index) {
 	var txHash = txHash;
 	var index = index;
@@ -12,5 +10,23 @@ function UTXO(txHash, index) {
 		return index;
 	}
 
-	
+	/**
+     * Compares this UTXO to the one specified by {@code other}, considering them equal if they have
+     * {@code txHash} arrays with equal contents and equal {@code index} values
+     */
+	this.equals = function (other) {
+		if (other == null) {
+			return false;
+		}
+		if (!(other instanceof UTXO)) {
+			return false;
+		}
+
+		utxo = other;
+		hash = utxo.getTxHash();
+		in = utxo.getIndex();
+		if (hash.length !== txHash.length || index !== in) {
+            return false;
+		}
+	}
 }
