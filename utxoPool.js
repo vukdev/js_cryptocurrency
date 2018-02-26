c5de708848a5d376d9b82ce7db9a98430c1a265f
radisadev <radisadev@gmail.com>
2018-02-26 21:47:53 +0100

    Add constructor for UTXOPool

--- utxoPool.js
+++ utxoPool.js
@@ -1,3 +1,11 @@
-function UTXOPool() {
+function UTXOPool(uPool) {
+	var H;
 
+	if (uPool instanceof UTXOPool) {
+		H = uPool.map(function (item) {
+			return Object.assign({}, item);
+		});
+	} else {
+		H = [];
+	}
 }
